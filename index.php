6299b0a3c5bf41d69cbaade0bc482c9e1dac30ec
monica-reverte <monicareverte24@gmail.com>
2023-02-02 12:22:53 +0100

    pull

--- index.php
+++ index.php
@@ -16,11 +16,11 @@ include_once "user.php";
 <body>
 
     <header>
-        <navbar class="navbar__header">
-            <div class="navbar__logo">
-                <img class="navbar__img" src="./assets/Mi_proyecto.png">
+        <navbar class="navbarheader">
+            <div class="navbarlogo">
+                <img class="navbarimg" src="./assets/Mi_proyecto.png">
             </div>
-            <p class="navbar__title">Landscapers</p>
+            <p class="navbartitle">Landscapers</p>
             <div class="navbar__button">
                 <button class="log-in">Log In</button>
                 <button class="register">Register</button>
@@ -31,19 +31,20 @@ include_once "user.php";
     <main>
         <section class="grid-container">
             <div class="route">
-                <img class="route__img" src="https://images.unsplash.com/photo-1619126208243-1a683b78d882?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80">
-                <article class="route__text">Route 1 text</article>
+                <img class="routeimg" src="https://images.unsplash.com/photo-1619126208243-1a683b78d882?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80%22%3E">
+                <p class="text">Text related to the image</p>
+<button id="readMore">Read More</button>
             </div>
             <div class="route">
-                <img class="route__img" src="https://images.unsplash.com/photo-1621847468516-1ed5d0df56fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80">
-                <article class="route__text">Route 2 text</article>
+                <img class="routeimg" src="https://images.unsplash.com/photo-1621847468516-1ed5d0df56fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80%22%3E">
+                <article class="routetext">Route 2 text</article>
             </div>
             <div class="route">
-                <img class="route__img" src="https://images.unsplash.com/photo-1610088948419-28cc15f5a397?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=654&q=80">
-                <article class="route__text">Route 3 text</article>
+                <img class="routeimg" src="https://images.unsplash.com/photo-1610088948419-28cc15f5a397?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=654&q=80%22%3E">
+                <article class="routetext">Route 3 text</article>
             </div>
             <div class="route">
-                <img class="route__img" src="https://images.unsplash.com/photo-1603984973710-e915353b35fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80">
+                <img class="routeimg" src="https://images.unsplash.com/photo-1603984973710-e915353b35fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80%22%3E">
                 <article class="route__text">Route 4 text</article>
             </div>
         </section>
@@ -52,6 +53,6 @@ include_once "user.php";
     <footer>
         <p>LandScapers © 2023</p>
     </footer>
-    
+
 </body>
 </html>
