0d67c60e3427097656614fc95a0ecd98e2ee7dc9
Edgar Arcos <edgararcosmartin@gmail.com>
2023-02-07 16:51:51 +0000

    Posts terminados

---
+++ posts/deletepost.php
@@ -0,0 +1,17 @@
+<?php
+require '../config/datapost.php';
+
+if (isset($_POST['ID']) && is_numeric($_POST['ID'])) {
+  $id = $conn->real_escape_string($_POST['ID']);
+
+  $sql = "DELETE FROM posts WHERE ID = $id";
+
+  if ($conn->query($sql)) {
+    header('Location: ../indexuser1.php');
+  } else {
+    // handle the error
+  }
+} else {
+  // handle the error
+}
+?>
