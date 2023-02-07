e92601efdfe93d22cefb03bb904c59da8c1ac443
Edgar Arcos <edgararcosmartin@gmail.com>
2023-02-07 13:39:38 +0000

    Revert "Savw"
    This reverts commit 178e37bfd46ca4622da42559a9815dca2c364c09.

--- posts/savepost.php
+++ posts/savepost.php
@@ -7,5 +7,8 @@ $content = $conn->real_escape_string($_POST['content']);
 $route = $conn->real_escape_string($_POST['route']);
 
 $sql = "INSERT INTO posts (title, content, id_route, timer) VALUES ('$title', '$content', $route, NOW())";
+if($conn->query($sql)) {
+    $id = $conn->insert_id;
+}
 
 header('Location: ../indexuser1.php');
